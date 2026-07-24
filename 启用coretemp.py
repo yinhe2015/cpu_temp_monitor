3e508a052f393524fbc8ce3315fa25077175de78
yinhe2015 <zhtxiii@gmail.com>
2026-07-24 10:48:24 +0800

    默认提交

---
+++ 启用coretemp.py
@@ -0,0 +1,22 @@
+import subprocess
+from 路径与日志记录 import 日志
+
+模块名 = 'coretemp'
+查看模块命令 = ['lsmod']
+启用模块命令 = ['modprobe', 模块名]
+
+def 是否加载() -> bool:
+    进程 = subprocess.check_call(查看模块命令, capture_output=True, text=True)
+    return 模块名 in 进程.stdout
+
+def 启用模块() -> None:
+    try:
+        subprocess.check_call(启用模块命令)
+        日志.信息(f'模块 {模块名} 启用成功')
+    except subprocess.CalledProcessError:
+        日志.异常(f'模块 {模块名} 启用失败')
+
+def 检查并启用模块() -> None:
+    if not 是否加载():
+        日志.警告(f'模块 {模块名} 未加载, 尝试启用')
+        启用模块()
