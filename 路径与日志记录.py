1338053a84f47bfaded3bdffbc30414cca70067b
yinhe2015 <zhtxiii@gmail.com>
2026-07-24 17:48:42 +0800

    默认提交

--- 路径与日志记录.py
+++ 路径与日志记录.py
@@ -7,7 +7,7 @@ import os
 数据目录 = os.path.join(主目录, '数据')
 os.makedirs(数据目录, exist_ok=True)
 
-日志路径 = os.path.join(主目录, '日志.log')
+日志路径 = os.path.join(数据目录, '日志.log')
 温度记录文件 = os.path.join(数据目录, '温度记录.csv')
 
 日志 = 打开日志(日志路径, 输出=True)
